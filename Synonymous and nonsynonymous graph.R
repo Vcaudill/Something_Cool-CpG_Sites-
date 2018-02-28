ca1050b17c4a19e82f903167d27069c391456633
Sarina Qin <sq134679@gmail.com>
2018-02-28 14:03:05 -0800

    Syn and nonsyn graph with comments

--- Synonymous and nonsynonymous graph.R
+++ Synonymous and nonsynonymous graph.R
@@ -1,101 +1,76 @@
+#Read in CSV file
 Dengue<-read.csv("DengueVirus1.fasta_pruned.mu.trim05_DF.csv")
-
+#select for only synonymous sites in Dengue file
 synonymous<-which(Dengue$TypeOfSite=='syn')
-
+#subset data into synonymous sites and designated wildtype nucleotide
 synonymousA<-subset(Dengue, wtnt=='a' & TypeOfSite=='syn')
 synonymousT<-subset(Dengue, wtnt=='t' & TypeOfSite=='syn') 
+#binds synonymous subset data into a new dataset
 synonymous<-rbind(synonymousA, synonymousT)
+#select for only nonsynonymous sites in Dengue file
 nonsynonymous<-which(Dengue$TypeOfSite=='nonsyn')
-
+#subset data into nonsynonymous sites and designated wildtypes
 nonsynonymousA<-subset(Dengue, wtnt=='a' & TypeOfSite=='nonsyn')
 nonsynonymousT<-subset(Dengue, wtnt=='t' & TypeOfSite=='nonsyn')
+#binds nonsynonymous subset data into a new dataset
 nonsynonymous<-rbind(nonsynonymousA, nonsynonymousT)
-
+#subset data into synonymous sites and designated wildtypes
 synonymousC<-subset(Dengue, wtnt=='c' & TypeOfSite=='syn')
 synonymousG<-subset(Dengue, wtnt=='g' & TypeOfSite=='syn')
-
+#subset data into nonsynonymous sites and designated wildtypes
 nonsynonymousC<-subset(Dengue, wtnt=='c' & TypeOfSite=='nonsyn')
 nonsynonymousG<-subset(Dengue, wtnt=='g' & TypeOfSite=='nonsyn')
 
-# hist(Dengue$makesCpG[synonymous],prob=TRUE,breaks=20,main="Synoymous Histogram", xlab= "CpG",ylab= "Number of CpG Sites",col= "black")
-# hist(Dengue$makesCpG[nonsynonymous],prob=TRUE,breaks=20,main="Non-Synoymous Histogram", xlab= "CpG",ylab= "Number of CpG Sites",col= "red")
-# 
-# 
-# hist(Dengue$MeanFreq[synonymous],
-#      prob=TRUE,breaks=20,main="Synoymous Nonsynoymous Histogram", 
-#      xlab= "Mean Frequency",ylab= "Mutation Frequency",col= rgb(1,0,0,0.5))
-# hist(Dengue$MeanFreq[nonsynonymous],
-#      prob=TRUE,breaks=20,main="Non-Synoymous Histogram", 
-#      xlab= "Mean Frequnecy",ylab= "Mutation Frequency",
-#      col= rgb(0,0,1,0.5), add=TRUE)
-# lines(density(Dengue$MeanFreq[synonymous]),col="red",lwd=2)
-# lines(density(Dengue$MeanFreq[nonsynonymous]),col="blue",lwd=2)
-# 
-# 
-# hist(nonsynonymousA$MeanFreq,
-#      prob=TRUE,breaks=20,main="Synoymous Nonsynoymous Histogram", 
-#      xlab= "Mean Frequency",ylab= "Mutation Frequency",col= rgb(1,0,0,0.5))
-# hist(synonymousA$MeanFreq,
-#      prob=TRUE,breaks=20,main="Non-Synoymous Histogram", 
-#      xlab= "Mean Frequnecy",ylab= "Mutation Frequency",
-#      col= rgb(0,0,1,0.5), add=TRUE)
-# 
-# hist(nonsynonymousT$MeanFreq,
-#      prob=TRUE,breaks=20,main="Synoymous Nonsynoymous Histogram", 
-#      xlab= "Mean Frequency",ylab= "Mutation Frequency",col= rgb(1,0,0,0.5))
-# hist(synonymousT$MeanFreq,
-#      prob=TRUE,breaks=20,main="Non-Synoymous Histogram", 
-#      xlab= "Mean Frequnecy",ylab= "Mutation Frequency",
-#      col= rgb(0,0,1,0.5), add=TRUE)
-# plot(synonymousA$num,synonymousA$MeanFreq, col = "red")
-# par(new=TRUE)
-# plot(nonsynonymousA$num,nonsynonymousA$MeanFreq, col = "blue")
-# par(new=TRUE)
-# plot(synonymousT$num,synonymousT$MeanFreq, col = "green")
-# par(new=TRUE)
-# plot(nonsynonymousT$num,nonsynonymousT$MeanFreq, col = "purple")
-# par(new=TRUE)
-# plot(synonymous$num,synonymous$MeanFreq, col = "red")
-# par(new=TRUE)
-# plot(nonsynonymous$num,nonsynonymous$MeanFreq)
+#A and T synonymous and nonsynonymous graphs
 
-layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
-plot(synonymousA$num,log10(synonymousA$MeanFreq+.000001), col = "red",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous A", pch=2)
+#splits the graph into 4 sections, 2 rows 2 columns 
+layout(matrix(c(1,2,3,4), nrow=2, byrow = TRUE))
+#log meanfreq to get rid of 0's and make graph easier to read
+plot(synonymousA$num,log10(synonymousA$MeanFreq+.000001), col = "red",xaxt="n", 
+     #pch creates the shape of the plotted points
+     yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous A", pch=2)
+#abline creates a line where the mean of all the points are located
 abline(h=mean(log10(synonymousA$MeanFreq+.000001)))
-legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymousT"),col="red", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+#creates a legend:inset is coordinates where the legend lies
 
+legend("topleft",inset=c(0,-0.45), legend=c
+       ("synonymous A"),col="red", horiz=TRUE, 
+#lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
+       lty=1, cex=0.8,xpd=TRUE,bty='n')
+#plots continue 
 plot(nonsynonymousA$num,log10(nonsynonymousA$MeanFreq+.000001), col = "blue",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous A", pch = 5)
 abline(h=mean(log10(nonsynonymousA$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymousA"),col="blue", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("nonsynonymous A"),col="blue", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(synonymousT$num,log10(synonymousT$MeanFreq+.000001), col = "green",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous T")
 abline(h=mean(log10(synonymousT$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("synonymousT"),col="green", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("synonymous T"),col="green", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(nonsynonymousT$num,log10(nonsynonymousT$MeanFreq+.000001), col = "purple",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous T", pch=0)
 abline(h=mean(log10(nonsynonymousT$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymousT"),col="purple", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("nonsynonymous T"),col="purple", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
+#Plot C and G synonymous and nonsynonymous graphs
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
+plot(synonymousC$num,log10(synonymousC$MeanFreq+.000001), col = "magenta",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous C", pch=7)
+abline(h=mean(log10(synonymousC$MeanFreq+.000001)))
+legend("topleft",inset=c(0,-0.45), legend=c
+       ("synonymous C"),col="magenta", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
+plot(nonsynonymousC$num,log10(nonsynonymousC$MeanFreq+.000001), col = "cyan",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous C", pch = 9)
+abline(h=mean(log10(nonsynonymousC$MeanFreq+.000001)))
+legend("topleft",inset=c(0,-0.45), legend=c
+       ("nonsynonymous C"),col="cyan", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
+plot(synonymousG$num,log10(synonymousG$MeanFreq+.000001), col = "black",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous G",pch=13)
+abline(h=mean(log10(synonymousG$MeanFreq+.000001)))
+legend("topleft",inset=c(0,-0.45), legend=c
+       ("synonymous G"),col="black", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
 
-# plot(synonymousA$num,log10(synonymousA$MeanFreq+.000001), 
-#      col = "red",xaxt="n", yaxt="n",xlab="", ylab="", pch=2)
-# abline(h=mean(log10(synonymousA$MeanFreq+.000001)))
-# par(new=TRUE)
-# plot(nonsynonymousA$num,log10(nonsynonymousA$MeanFreq+.000001), col = "blue",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous and Nonsynonymous A", pch=5)
-# abline(h=mean(log10(nonsynonymousA$MeanFreq+.000001)))
-# legend("topleft",inset=c(0,-0.45), legend=c("synonymousA", "nonsynonymousA"),col=c("red", "blue"), horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
-# plot(synonymousT$num,synonymousT$MeanFreq, col = "green",,xaxt="n", yaxt="n",xlab="", ylab="")
-# par(new=TRUE)
-# plot(nonsynonymousT$num,nonsynonymousT$MeanFreq, col = "purple",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="Synonymous and Nonsynonymous T", pch=0)
-# legend("topleft",inset=c(0,-0.45), legend=c("synonymousT", "nonsynonymousT"),col=c("green", "purple"), horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
-# 
-# syn_Cpg<-Dengue$MeanFreq[synonymous]
-# nonsyn_Cpg<-Dengue$MeanFreq[nonsynonymous]
-# wilcox.test(syn_Cpg,nonsyn_Cpg)
-
+plot(nonsynonymousT$num,log10(nonsynonymousT$MeanFreq+.000001), col = "yellow",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous G", pch=14)
+abline(h=mean(log10(nonsynonymousT$MeanFreq+.000001)))
+legend("topleft",inset=c(0,-0.45), legend=c
+       ("nonsynonymous G"),col="yellow", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
