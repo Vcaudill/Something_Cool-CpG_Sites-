12ffdeb96e96305484467e5605beb77f5e7274d9
Sarina Qin <sq134679@gmail.com>
2018-03-02 14:27:07 -0800

    Created function

--- Synonymous and nonsynonymous graph.R
+++ Synonymous and nonsynonymous graph.R
@@ -1,25 +1,26 @@
 #Read in CSV file
 Dengue<-read.csv("DengueVirus1.fasta_pruned.mu.trim05_DF.csv")
-#select for only synonymous sites in Dengue file
-synonymous<-which(Dengue$TypeOfSite=='syn')
+SynNonsynAT<-function(Virus){
+#select for only synonymous sites in Virus file
+synonymous<-which(Virus$TypeOfSite=='syn')
 #subset data into synonymous sites and designated wildtype nucleotide
-synonymousA<-subset(Dengue, wtnt=='a' & TypeOfSite=='syn')
-synonymousT<-subset(Dengue, wtnt=='t' & TypeOfSite=='syn') 
+synonymousA<-subset(Virus, wtnt=='a' & TypeOfSite=='syn')
+synonymousT<-subset(Virus, wtnt=='t' & TypeOfSite=='syn') 
 #binds synonymous subset data into a new dataset
 synonymous<-rbind(synonymousA, synonymousT)
-#select for only nonsynonymous sites in Dengue file
-nonsynonymous<-which(Dengue$TypeOfSite=='nonsyn')
+#select for only nonsynonymous sites in Virus file
+nonsynonymous<-which(Virus$TypeOfSite=='nonsyn')
 #subset data into nonsynonymous sites and designated wildtypes
-nonsynonymousA<-subset(Dengue, wtnt=='a' & TypeOfSite=='nonsyn')
-nonsynonymousT<-subset(Dengue, wtnt=='t' & TypeOfSite=='nonsyn')
+nonsynonymousA<-subset(Virus, wtnt=='a' & TypeOfSite=='nonsyn')
+nonsynonymousT<-subset(Virus, wtnt=='t' & TypeOfSite=='nonsyn')
 #binds nonsynonymous subset data into a new dataset
 nonsynonymous<-rbind(nonsynonymousA, nonsynonymousT)
 #subset data into synonymous sites and designated wildtypes
-synonymousC<-subset(Dengue, wtnt=='c' & TypeOfSite=='syn')
-synonymousG<-subset(Dengue, wtnt=='g' & TypeOfSite=='syn')
+synonymousC<-subset(Virus, wtnt=='c' & TypeOfSite=='syn')
+synonymousG<-subset(Virus, wtnt=='g' & TypeOfSite=='syn')
 #subset data into nonsynonymous sites and designated wildtypes
-nonsynonymousC<-subset(Dengue, wtnt=='c' & TypeOfSite=='nonsyn')
-nonsynonymousG<-subset(Dengue, wtnt=='g' & TypeOfSite=='nonsyn')
+nonsynonymousC<-subset(Virus, wtnt=='c' & TypeOfSite=='nonsyn')
+nonsynonymousG<-subset(Virus, wtnt=='g' & TypeOfSite=='nonsyn')
 
 #A and T synonymous and nonsynonymous graphs
 
@@ -41,36 +42,40 @@ legend("topleft",inset=c(0,-0.45), legend=c
 plot(nonsynonymousA$num,log10(nonsynonymousA$MeanFreq+.000001), col = "blue",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous A", pch = 5)
 abline(h=mean(log10(nonsynonymousA$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymous A"),col="blue", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
- 
+       ("nonsynonymous A"),col="blue", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
+
 plot(synonymousT$num,log10(synonymousT$MeanFreq+.000001), col = "green",xlab="Position Number", ylab="MeanFreq",main="Synonymous T")
 abline(h=mean(log10(synonymousT$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("synonymous T"),col="green", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("synonymous T"),col="green", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(nonsynonymousT$num,log10(nonsynonymousT$MeanFreq+.000001), col = "purple",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous T", pch=0)
 abline(h=mean(log10(nonsynonymousT$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymous T"),col="purple", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
-
+       ("nonsynonymous T"),col="purple", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
+}
+SynNonsynCG<-function(Virus){
 #Plot C and G synonymous and nonsynonymous graphs
 layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
 plot(synonymousC$num,log10(synonymousC$MeanFreq+.000001), col = "magenta",xlab="Position Number", ylab="MeanFreq",main="Synonymous C", pch=7)
 abline(h=mean(log10(synonymousC$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("synonymous C"),col="magenta", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("synonymous C"),col="magenta", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(nonsynonymousC$num,log10(nonsynonymousC$MeanFreq+.000001), col = "cyan",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous C", pch = 9)
 abline(h=mean(log10(nonsynonymousC$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymous C"),col="cyan", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("nonsynonymous C"),col="cyan", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(synonymousG$num,log10(synonymousG$MeanFreq+.000001), col = "black",xlab="Position Number", ylab="MeanFreq",main="Synonymous G",pch=13)
 abline(h=mean(log10(synonymousG$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("synonymous G"),col="black", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("synonymous G"),col="black", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
 
 plot(nonsynonymousT$num,log10(nonsynonymousT$MeanFreq+.000001), col = "yellow",xlab="Position Number", ylab="MeanFreq",main="NonSynonymous G", pch=14)
 abline(h=mean(log10(nonsynonymousT$MeanFreq+.000001)))
 legend("topleft",inset=c(0,-0.45), legend=c
-       ("nonsynonymous G"),col="yellow", horiz=TRUE, lty=1:1, cex=0.8,xpd=TRUE,bty='n')
+       ("nonsynonymous G"),col="yellow", horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
+}
+SynNonsynAT(Dengue)
+SynNonsynCG(Dengue)
