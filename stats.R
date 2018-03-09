8273a8fdeee9e00dd13f853bf02b4c1d70cb58b2
Vcaudill <vcaudill@mail.sfsu.edu>
2018-03-09 10:12:50 -0800

    added more test

--- stats.R
+++ stats.R
@@ -11,6 +11,10 @@ array1 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn' & data$makesCp
 array2 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn' & data$makesCpG == 0]
 array3 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 1]
 array4 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 0]
+syna = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn']
+nonsyna = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn']
+CpGa = data$MeanFreq[data$wtnt =="a" & data$makesCpG == 1]
+nonCpGa = data$MeanFreq[data$wtnt =="a" &  data$makesCpG == 0]
 
 print("For a: Compairng makes CpG with does not make CpG. Wilcox test less")
 print(wilcox.test(array1, array2, alternative='less'))
@@ -18,13 +22,20 @@ print("For a: Compairng makes syn mutation to nonsyn mutation. Both makes CpG, W
 print(wilcox.test(array1, array3, alternative='greater'))
 print("For a: Compairng makes syn mutation to nonsyn mutation. No CpG made, Wilcox test greater")
 print(wilcox.test(array2, array4, alternative='greater'))
+print("For a: Compairng syn to nonsyn. Wilcox test greater")
+print(wilcox.test(syna, nonsyna, alternative='greater'))
+print("For a: Compairng CpG to no CpG. Wilcox test less")
+print(wilcox.test(CpGa, nonCpGa, alternative='less'))
 
 
 array5 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn' & data$makesCpG == 1]
 array6 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn' & data$makesCpG == 0]
 array7 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 1]
 array8 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 0]
-
+synt = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn']
+nonsynt = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn']
+CpGt = data$MeanFreq[data$wtnt =="t" & data$makesCpG == 1]
+nonCpGt = data$MeanFreq[data$wtnt =="t" &  data$makesCpG == 0]
 
 print("For t: Compairng makes CpG with does not make CpG. Wilcox test less")
 print(wilcox.test(array5, array6, alternative='less'))
@@ -32,51 +43,36 @@ print("For t: Compairng makes syn mutation to nonsyn mutation. Both makes CpG, W
 print(wilcox.test(array5, array7, alternative='greater'))
 print("For t: Compairng makes syn mutation to nonsyn mutation. No CpG made, Wilcox test greater")
 print(wilcox.test(array5, array8, alternative='greater'))
+print("For t: Compairng syn to nonsyn. Wilcox test greater")
+print(wilcox.test(synt, nonsynt, alternative='greater'))
+print("For t: Compairng CpG to no CpG. Wilcox test less")
+print(wilcox.test(CpGt, nonCpGt, alternative='less'))
 
+
+sync = data$MeanFreq[data$wtnt =="c" & data$TypeOfSite == 'syn']
+nonsync = data$MeanFreq[data$wtnt =="c" & data$TypeOfSite == 'nonsyn']
+print("For c: Compairng syn to nonsyn. Wilcox test greater")
+print(wilcox.test(sync, nonsync, alternative='greater'))
+
+syng = data$MeanFreq[data$wtnt =="g" & data$TypeOfSite == 'syn']
+nonsyng = data$MeanFreq[data$wtnt =="g" & data$TypeOfSite == 'nonsyn']
+print("For g: Compairng syn to nonsyn. Wilcox test greater")
+print(wilcox.test(syng, nonsyng, alternative='greater'))
 
 }
 Wilcox_test(data)
 
-# #subset into two groups yes makes cpg and no cpg
-# cpg.y<-subset(Dvirus, makesCpG==1)
-# cpg.n<-subset(Dvirus, makesCpG==0)
-# #subset further into letters 
-# AC<-subset(cpg.y, wtnt=='a')
-# ANC<-subset(cpg.n, wtnt=='a')
-# GC<-subset(cpg.y, wtnt=='g')
-# GNC<-subset(cpg.n, wtnt=='g')
-# TC<-subset(cpg.y, wtnt=='t')
-# TNC<-subset(cpg.n, wtnt=='t')
-# CC<-subset(cpg.y, wtnt=='c')
-# CNC<-subset(cpg.n, wtnt=='c')
+# low <- c(1,1,2,2,3,3,4,4,5,5)
+# high <- c(6,6,7,7,8,8,9,9,10,10, 12, 78)
 # 
-# #for wilcox test
-# CpGdataA<-data.frame(CpG=c(rep("Y",times=length(AC$MeanFreq)),rep("N",times=length(ANC$MeanFreq))), MeanFreq=c(AC$MeanFreq,ANC$MeanFreq))
-# CpGdataANC<-data.frame(CpG=c(rep("Y",times=length(ANC$MeanFreq)),rep("N",times=length(AC$MeanFreq))), MeanFreq=c(ANC$MeanFreq,AC$MeanFreq))
-# 
-# CpGdataG<-data.frame(CpG=c(rep("Y",times=length(GC$MeanFreq)),rep("N",times=length(GNC$MeanFreq))), MeanFreq=c(GC$MeanFreq,GNC$MeanFreq))
-# CpGdataGNC<-data.frame(CpG=c(rep("Y",times=length(GNC$MeanFreq)),rep("N",times=length(GC$MeanFreq))), MeanFreq=c(GNC$MeanFreq,GC$MeanFreq))
-# 
-# CpGdataT<-data.frame(CpG=c(rep("Y",times=length(TC$MeanFreq)),rep("N",times=length(TNC$MeanFreq))), MeanFreq=c(TC$MeanFreq,TNC$MeanFreq))
-# CpGdataTNC<-data.frame(CpG=c(rep("Y",times=length(TNC$MeanFreq)),rep("N",times=length(TC$MeanFreq))), MeanFreq=c(TNC$MeanFreq,TC$MeanFreq))
-# 
-# CpGdataC<-data.frame(CpG=c(rep("Y",times=length(CC$MeanFreq)),rep("N",times=length(CNC$MeanFreq))), MeanFreq=c(CC$MeanFreq,CNC$MeanFreq))
-# CpGdataCNC<-data.frame(CpG=c(rep("Y",times=length(CNC$MeanFreq)),rep("N",times=length(CC$MeanFreq))), MeanFreq=c(CNC$MeanFreq,CC$MeanFreq))
-# 
-# 
-# # test for A and T what are the differences? 
-# # will not work if everthing is no or yes, must have two levels?
-# wilcox.test(MeanFreq ~ CpG, data=CpGdataA, alternative='greater', paired = FALSE)
-# wilcox.test(MeanFreq ~ CpG, data=CpGdataANC, alternative='greater', paired = FALSE)
-# 
-# wilcox.test(MeanFreq ~ CpG, data=CpGdataT, alternative='greater', paired = FALSE)
-# wilcox.test(MeanFreq ~ CpG, data=CpGdataTNC, alternative='greater', paired = FALSE)
-
-
-
-
-
-
+# wilcox.test(low, high, alternative='less')
+# wilcox.test(low, high, alternative='greater')
+# wilcox.test(low, high)
+# # if p<.05 is significant
+# num = 3
+# low2= rep(1,num)
+# high2 = rep (3, num)
+# wilcox.test(low2, high2, alternative='less')
 
 
 
