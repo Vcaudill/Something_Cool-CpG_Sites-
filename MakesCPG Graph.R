f8cfe4a009e333cb7b55658459c424dcf4f0c063
Sarina Qin <sq134679@gmail.com>
2018-03-09 12:29:00 -0800

    fixed

--- MakesCPG Graph.R
+++ MakesCPG Graph.R
@@ -21,16 +21,16 @@ legend("topleft",inset=c(0,1.01), legend=c
 #overlaps second graph on top of first graph
 par(new=TRUE)
 #plots second graph No CPG A. xaxt gets rid of x axis label yaxt gets rid of y
-plot(NoCPGA$num,log10(NoCPGA$MeanFreq+.000001), col=c("red","blue"),xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq", pch=5)
-abline(h=mean(log10(NoCPGA$MeanFreq+.000001)),col="blue")
+plot(NoCPGA$num,NoCPGA$MeanFreq+.000001,log='y', col=c("red","blue"),xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq", pch=5)
+abline(h=mean(NoCPGA$MeanFreq+.000001),col="blue")
 
-plot(CPGT$num,log10(CPGT$MeanFreq+.000001), col = "green",xlab="Position Number", ylab="MeanFreq",main="CPG & No CPG T")
-abline(h=mean(log10(CPGT$MeanFreq+.000001)),col = "green")
+plot(CPGT$num,CPGT$MeanFreq+.000001,log='y', col = "green",xlab="Position Number", ylab="MeanFreq",main="CPG & No CPG T")
+abline(h=mean(CPGT$MeanFreq+.000001),col = "green")
 legend("topleft",inset=c(0,1.01), legend=c
        ("CPG T","No CPG T"),col=c("green","purple"), horiz=TRUE, lty=1, cex=0.8,xpd=TRUE,bty='n')
 par(new=TRUE)
-plot(NoCPGT$num,log10(NoCPGT$MeanFreq+.000001), col="purple",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq", pch=0)
-abline(h=mean(log10(NoCPGT$MeanFreq+.000001)),col="purple")
+plot(NoCPGT$num,NoCPGT$MeanFreq+.000001,log='y', col="purple",xaxt="n", yaxt="n",xlab="Position Number", ylab="MeanFreq", pch=0)
+abline(h=mean(NoCPGT$MeanFreq+.000001),col="purple")
 }
  
 CPGNoCPGAT(Dengue)
