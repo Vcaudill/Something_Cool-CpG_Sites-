cf44c15cdd1bd37109e0f94a8b2e149b9b4957fe
Sarina Qin <sq134679@gmail.com>
2018-04-23 13:22:00 -0700

    changes

--- Sarina.R
+++ Sarina.R
@@ -5,7 +5,7 @@ setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
 # DataSet <-read.fasta("DengueVirus1.fasta_pruned.mu.trim05.txt")
 source("MeaFreq.R")
 # must place your file as a txt takes a few minutes 
-DF<-meanFreq('DengueVirus2.fasta_pruned.mu.trim05.txt')
+DF<-meanFreq('EnterovirusA_VP1.fasta_pruned.mu.trim05.txt')
 DF$wtnt<-as.character(DF$wtnt)
 
 source("WTAA.R")
@@ -14,7 +14,7 @@ DF<-getWTAA(DF)
 source("MUTAA.R")
 DF<-getMUTAA(DF)
 
-source("Drastic_AA_Change.R")
+source("Drastic_AA_Change.R") 
 DF<-big_aa_change(DF)
 
 source("SynNonSyn.R")
@@ -24,17 +24,21 @@ source("CPG_Function.R")
 DF<-CPG_site(DF)
 
 # graphs 
-
+pdf('Synon_Nonsyn_Graph.pdf',width = 7, height = 5)
 source("Synonymous and nonsynonymous graph.R")
+dev.off()
 SynNonsynAT(DF)
 SynNonsynCG(DF)
 
-
+pdf('CPG_NonCPG_AT_Graph.pdf',width = 7, height = 5)
 source("MakesCPG Graph.R")
+dev.off()
 CPGNoCPGAT(DF)
 
 source("CPG_Syn_Nonsyn_graph.R")
+pdf('comparing_CpG_Syn_Graph.pdf',width = 7, height = 5)
 comparing_CpG_Syn_Nonsyn (DF)
+dev.off()
 
 # Wilcox test
 source("stats.R")
