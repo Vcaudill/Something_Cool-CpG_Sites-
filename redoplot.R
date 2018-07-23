618c16663874a2fe734fe1139abd8ae62ed0582e
Sarina Qin <sq134679@gmail.com>
2018-07-23 12:18:56 -0700

    resolution fixed

--- redoplot.R
+++ redoplot.R
@@ -3,7 +3,6 @@
 
 data<-read.csv("DengueVirus1.fasta_pruned.mu.trim05_DF.csv")
 
-
 comparing_CpG_Syn_Nonsyn = function(data){
 
   #subset into two groups yes makes cpg and no cpg
@@ -150,11 +149,12 @@ comparing_CpG_Syn_Nonsyn = function(data){
   library(plotrix)
   library(sfsmisc)
   
-  png("Den1_jitter.png", res = 30000)
+  #png("Den1_jitter1.png", width = 405, height = 405, units = "px")
+  
   #layout(matrix(c(1,2,3,4), nrow=2, byrow = TRUE))
+  
+  png("Dengue1.png", width = 6.75, height = 6.75, units = "in", res= 300)
   par(mfrow=c(2,2)) 
-  #pdf("redoplottest.pdf", width = 6.75, height= 3)
-
   palette(alpha(c("#99FF99","#9999FF","#FF9900","#FF3300"),0.3))
   #graph_color = palette(c("#99FF99","#9999FF","#FF9900","#FF3300"))
  
@@ -187,7 +187,7 @@ comparing_CpG_Syn_Nonsyn = function(data){
   
   palette(alpha(c("#99FF99","#FF9900"),0.3)) 
   plot(jitter(AllC$graphit, 0.6),AllC$MeanFreq+ 0.0001,log='y',col=factor(AllC$graphit),pch=16,main="C",xlab = "Mutation Type", ylab = "Mutation Frequency",  xlim=c(0.5,4.5),yaxt="n", xaxt = "n")
-  
+ 
   points(AllC$graphit, AllC$mean_val, col= factor(AllC$graphit), pch=19, cex = 3)
   arrows(AllC$graphit, AllC$LCLS, AllC$graphit, AllC$UCLS, length=0.3,lwd = 5, angle=90, code=3, col= factor(AllC$graphit) )
   eaxis(2,at=c(10^-3,10^-2,10^-1,10^0),cex.axis=1.1)
