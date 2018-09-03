bbfcaae0994468e8c92fb8671ece43c94bcbb95a
RyanWinstead <ryanwinstead33@yahoo.com>
2018-09-03 16:51:01 -0700

    yeaaass

--- R_scripts/Tables/HowToMakeTables.R
+++ R_scripts/Tables/HowToMakeTables.R
@@ -1,13 +1,13 @@
 namelist=c("DengueVirus1", "DengueVirus2", "DengueVirus3", "DengueVirus4", "humanparainfluenzavirus1_F", "humanparainfluenzavirus1_HN", "humanparainfluenzavirus3_HN", "InfluenzaAvirus_HA_H1N1","InfluenzaAvirus_HA_H3N2", "InfluenzaAvirus_NA_H1N1", "InfluenzaAvirus_NA_H3N2","InfluenzaBvirus_HA", "InfluenzaBvirus_NA", "EnterovirusA_VP1", "EnterovirusA_VP2","EnterovirusB_VP1", "EnterovirusB_VP2","EnterovirusC_VP1","EnterovirusC_VP2","EnterovirusD_VP1", "BKpolyomavirus_VP1", "HumanBocavirus1_NS1", "HumanBocavirus1_VP1")
-
+#List of all virus names (name + .csv/Rda)
 
 Tables = function(truename){  
-  
   setwd("~/Desktop/Something_Cool-CpG_Sites-")
   truenameRda= paste(truename, ".Rda", sep="")
   setwd("~/Desktop/Something_Cool-CpG_Sites-/data/Rda")
   print(truenameRda)
   load(truenameRda)
+  #load data as DF
   # Wilcox test
   setwd("~/Desktop/Something_Cool-CpG_Sites-")
   Wilcox_test(DF, truename)
@@ -65,6 +65,7 @@ Wilcox_test = function(data, truename){
   pVals = c(pVals,format(wilcox.test(synt, nonsynt, alternative='greater')$p.value, nsmall = 6))
   
   Pvalues= c(pVals)
+  #save Pvalues into list
   
   setwd("~/Desktop/Something_Cool-CpG_Sites-")
   
