f886d58b4392877f9c716c01e02043d99d86b791
RyanWinstead <ryanwinstead33@yahoo.com>
2018-09-03 16:13:57 -0700

    Add files via upload

---
+++ R_scripts/Tables/HowToMakeTables.R
@@ -0,0 +1,143 @@
+namelist=c("DengueVirus1", "DengueVirus2", "DengueVirus3", "DengueVirus4", "humanparainfluenzavirus1_F", "humanparainfluenzavirus1_HN", "humanparainfluenzavirus3_HN", "InfluenzaAvirus_HA_H1N1","InfluenzaAvirus_HA_H3N2", "InfluenzaAvirus_NA_H1N1", "InfluenzaAvirus_NA_H3N2","InfluenzaBvirus_HA", "InfluenzaBvirus_NA", "EnterovirusA_VP1", "EnterovirusA_VP2","EnterovirusB_VP1", "EnterovirusB_VP2","EnterovirusC_VP1","EnterovirusC_VP2","EnterovirusD_VP1", "BKpolyomavirus_VP1", "HumanBocavirus1_NS1", "HumanBocavirus1_VP1")
+
+
+Tables = function(truename){  
+  
+  setwd("~/Desktop/Something_Cool-CpG_Sites-")
+  truenameRda= paste(truename, ".Rda", sep="")
+  setwd("~/Desktop/Something_Cool-CpG_Sites-/data/Rda")
+  print(truenameRda)
+  load(truenameRda)
+  # Wilcox test
+  setwd("~/Desktop/Something_Cool-CpG_Sites-")
+  Wilcox_test(DF, truename)
+}
+
+Wilcox_test = function(data, truename){
+  truenamepdf= paste(truename, ".pdf", sep="")
+  library(graphics)
+  library(dplyr)
+  library(plyr)
+  pVals = c()
+  shrtval = 0
+  options(scipen=999)
+  
+  array1 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn' & data$makesCpG == 1]
+  array2 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn' & data$makesCpG == 0]
+  array3 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 1]
+  array4 = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 0]
+  syna = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'syn']
+  nonsyna = data$MeanFreq[data$wtnt =="a" & data$TypeOfSite == 'nonsyn']
+  CpGa = data$MeanFreq[data$wtnt =="a" & data$makesCpG == 1]
+  nonCpGa = data$MeanFreq[data$wtnt =="a" &  data$makesCpG == 0]
+  
+  print("For a: Comparing makes CpG with noCpG (syn). Wilcox test less: red/blue")
+  print(wilcox.test(array1, array2, alternative='less'))
+  pVals = c(pVals,format(wilcox.test(array1, array2, alternative='less')$p.value, nsmall = 6))
+  print(pVals)
+  print("For a: Comparing CpG with noCpG (nonsyn). Wilcox test less: yellow/green")
+  print(wilcox.test(array3, array4, alternative='less'))
+  pVals = c(pVals,format(wilcox.test(array3, array4, alternative='less')$p.value, nsmall = 6))
+  print(pVals)
+  print("For a: Comparing  syn to nonsyn. Wilcox test greater red&blue vs yellow&green")
+  print(wilcox.test(syna, nonsyna, alternative='greater'))
+  print(wilcox.test(syna, nonsyna, alternative='greater')$p.value)
+  pVals = c(pVals,format(wilcox.test(syna, nonsyna, alternative='greater')$p.value, nsmall = 6))
+  print(pVals)
+  
+  array5 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn' & data$makesCpG == 1]
+  array6 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn' & data$makesCpG == 0]
+  array7 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 1]
+  array8 = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn' & data$makesCpG == 0]
+  synt = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'syn']
+  nonsynt = data$MeanFreq[data$wtnt =="t" & data$TypeOfSite == 'nonsyn']
+  CpGt = data$MeanFreq[data$wtnt =="t" & data$makesCpG == 1]
+  nonCpGt = data$MeanFreq[data$wtnt =="t" &  data$makesCpG == 0]
+  
+  print("For t: Comparing makes CpG with noCpG (syn). Wilcox test less: red/blue")
+  print(wilcox.test(array5, array6, alternative='less'))
+  pVals = c(pVals,format(wilcox.test(array5, array6, alternative='less')$p.value, nsmall = 6))
+  print("For t: Comparing CpG with noCpG (nonsyn). Wilcox test less: yellow/green")
+  print(wilcox.test(array7, array8, alternative='less'))
+  pVals = c(pVals,format(wilcox.test(array7, array8, alternative='less')$p.value, nsmall = 6))
+  print("For t: Comparing  syn to nonsyn. Wilcox test greater red&blue vs yellow&green")
+  print(wilcox.test(synt, nonsynt, alternative='greater'))
+  pVals = c(pVals,format(wilcox.test(synt, nonsynt, alternative='greater')$p.value, nsmall = 6))
+  
+  Pvalues= c(pVals)
+  
+  setwd("~/Desktop/Something_Cool-CpG_Sites-")
+  
+  makeTable(Pvalues, truenamepdf, truename)
+  
+}
+
+makeTable <- function(Pvalues,truenamepdf, truename){
+  options(scipen = 999)
+  options(warn=-1)
+  
+  setwd("~/Desktop/Something_Cool-CpG_Sites-/Tables")
+  pdf(truenamepdf, width = 7, height= 5)
+  col1 <- c("A-G", "T-C")
+  col2 <- c("Syn: CpG v NonCpG", "NonSyn: CpG v NonCpG", "Syn v NonSyn")
+  ycoor <- c(4*100/5+.7 , 3*100/5 + 5.1, 3*100/5 - 10, 2*100/5 -6.4, 1*100/5-1.3, 100/5-14- 2.9, 100)
+  ycoorb <- c(4*100/5+.7 , 3*100/5 + 5.1, 3*100/5 - 10.6, 2*100/5 -6.4, 1*100/5-1.3, 100/5-14- 2.9, 100)
+  df = data.frame(col1, col2, Pvalues)
+  
+  par(xpd=F)
+  plot(1, 2, xlim=c(0,100),ylim=c(0,100), col=0, xaxt="n", yaxt="n", xlab="", ylab="")
+  title(main = truename, family = "Times", adj = 0.5, cex.main= 2)
+  abline(v = 100/5)
+  abline(v = 2*100/3)
+  abline(h = 100-100/7 + 3)
+  abline(h = 100- 2*100/7+2)
+  abline(h = 100 - 3*100/7+1)
+  abline(h= 100 - 4*100/7 -1)
+  abline(h= 100 -5*100/7 - 2)
+  abline(h= 100 -6*100/7 - 3)
+  
+  
+  text(x=100/7- 6, y= 5*100/5-3, "Mutation Type")
+  text(x=3*100/7, y = 5*100/5-3, "Comparison")
+  text(x= 6*100/7, y=5*100/5-3, "P-Value")
+  rect(xleft = -4, xright = 100/5, ybottom =42, ytop =100-100/7+3 , col = "white")
+  text(x= 100/12, y= 3*100/5+5, "A-G", cex = 1.7, family = "Times")
+  rect(xleft = -4, xright = 100/5, ybottom =-4, ytop =42 , col = "white")
+  text(x= 100/12, y = 1*100/5 - 1, "T-C", cex = 1.7, family ='Times')
+  text(x= 3*100/7, y = ycoor[1], labels= col2[1])
+  text(x= 3*100/7, y = ycoor[2], labels= col2[2])
+  text(x= 3*100/7, y = ycoor[3], labels= col2[3])
+  text(x= 3*100/7, y = ycoor[4], labels= col2[1])
+  text(x= 3*100/7, y = ycoor[5], labels= col2[2])
+  text(x= 3*100/7, y = ycoor[6], labels= col2[3])
+  
+  num <- 1
+  for (i in Pvalues){
+    #i = format(i, nsmall = 6)
+    print(i)
+    
+    
+    library(scales)
+    if (i < 0.01){
+      a = 0.45
+      i = "< 0.01"
+    }
+    else if(i <0.05){
+      a = 0.25
+    }
+    else if(i >0.05){
+      a = 0.1
+    }
+    
+    rect(xleft = 2*100/3, xright = 200, ybottom = ycoorb[num]-7.3, ytop = ycoor[num]+8, col = alpha("deepskyblue1", a), border = col)
+    text(x= 6*100/7, y =ycoor[num], labels = i)
+    num = num + 1 
+    
+  }
+  print("end")
+  dev.off()
+}
+
+for(truename in namelist){
+  
+  Tables(truename)}
