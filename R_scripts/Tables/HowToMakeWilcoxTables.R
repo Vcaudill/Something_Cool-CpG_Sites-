90533b7b753f7c5ebff4ea2aa2ac4e2383f4d6e9
RyanWinstead <ryanwinstead33@yahoo.com>
2018-09-07 15:32:20 -0700

    wd changes

--- R_scripts/Tables/HowToMakeWilcoxTables.R
+++ R_scripts/Tables/HowToMakeWilcoxTables.R
@@ -1,20 +1,23 @@
 namelist=c("DengueVirus1", "DengueVirus2", "DengueVirus3", "DengueVirus4", "humanparainfluenzavirus1_F", "humanparainfluenzavirus1_HN", "humanparainfluenzavirus3_HN", "InfluenzaAvirus_HA_H1N1","InfluenzaAvirus_HA_H3N2", "InfluenzaAvirus_NA_H1N1", "InfluenzaAvirus_NA_H3N2","InfluenzaBvirus_HA", "InfluenzaBvirus_NA", "EnterovirusA_VP1", "EnterovirusA_VP2","EnterovirusB_VP1", "EnterovirusB_VP2","EnterovirusC_VP1","EnterovirusC_VP2","EnterovirusD_VP1", "BKpolyomavirus_VP1", "HumanBocavirus1_NS1", "HumanBocavirus1_VP1")
 #List of all virus names (name + .csv/Rda)
 
-Tables = function(truename){  
-  setwd("~/Desktop/Something_Cool-CpG_Sites-")
-  truenameRda= paste(truename, ".Rda", sep="")
-  setwd("~/Desktop/Something_Cool-CpG_Sites-/data/Rda")
-  print(truenameRda)
-  load(truenameRda)
+Tables = function(truename){ 
+  #truename ="DengueVirus1"
+  truenamecsv= paste(truename, ".csv", sep="")
+  print(truenamecsv)
+  DF<- read.csv(paste("data/csv/", truenamecsv, sep=""))
   #load data as DF
   # Wilcox test
-  setwd("~/Desktop/Something_Cool-CpG_Sites-")
+  
   Wilcox_test(DF, truename)
 }
 
 Wilcox_test = function(data, truename){
-  truenamepdf= paste(truename, ".pdf", sep="")
+  
+  truenamepdf= paste("Tables/",truename, ".pdf", sep="")
+  print(truenamepdf)
+  
+  
   #set output pdf file name
   library(graphics)
   library(dplyr)
@@ -69,7 +72,6 @@ Wilcox_test = function(data, truename){
   Pvalues= c(pVals)
   #save Pvalues into list
   
-  setwd("~/Desktop/Something_Cool-CpG_Sites-")
   
   makeTable(Pvalues, truenamepdf, truename)
   
@@ -81,7 +83,7 @@ makeTable <- function(Pvalues,truenamepdf, truename){
   options(warn=-1)
   #suppress warnings
   
-  setwd("~/Desktop/Something_Cool-CpG_Sites-/Tables")
+  #setwd("~/Desktop/Something_Cool-CpG_Sites-/Tables")
   #table construct
   pdf(truenamepdf, width = 7, height= 5)
   col1 <- c("A-G", "T-C")
