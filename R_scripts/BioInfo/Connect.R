fd42a5c58d121be8006b029e0666d5e6e39c5f7b
Jasmeen Kaur <jkaur1@mail.sfsu.edu>
2018-09-11 18:56:41 -0700

    source changes and setwd changed

--- R_scripts/BioInfo/Connect.R
+++ R_scripts/BioInfo/Connect.R
@@ -5,15 +5,15 @@
 # DataSet <-read.fasta("DengueVirus1.fasta_pruned.mu.trim05.txt")
 source("R_scripts/BioInfo/Freq.R")
 # must place your file as a txt takes a few minutes 
-setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/virus")
+#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/virus")
 virusname = 'EnterovirusB_VP2.fasta.mu.trim08'
 virusname = 'DengueVirus1.fasta_pruned.mu.trim05.txt'
-DF<-meanFreq(virusname)
+DF<-Freq(virusname)
 splitname<-unlist(strsplit(virusname,".fasta"))
 truename<-splitname[1]
 DF$wtnt<-as.character(DF$wtnt)
 DF$Virus<-(truename)
-setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
+#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
 
 # for Ryan 
 # make sure that M is the first amino acid check all virus
@@ -23,19 +23,19 @@ DF<-DF[c(start:(nrow(DF)-end)),]
 DF$num<-(1:nrow(DF))
 
 
-source("WTAA.R")
+source("R_scripts/BioInfo/WTAA_consensus.R")
 DF<-getWTAA(DF)
 
-source("MUTAA.R")
+source("R_scripts/BioInfo/MUTAA.R")
 DF<-getMUTAA(DF)
 
-source("Drastic_AA_Change.R")
+source("R_scripts/BioInfo/Drastic_AA_Change.R")
 DF<-big_aa_change(DF)
 
-source("SynNonSyn.R")
+source("R_scripts/BioInfo/SynNonSyn.R")
 DF<-synFunction(DF)
 
-source("CPG_Function.R")
+source("R_scripts/BioInfo/CPG_Function.R")
 DF<-CPG_site(DF)
 #How to save data
 truenameRda= paste(truename, ".Rda", sep="")
@@ -48,30 +48,30 @@ splitname<-unlist(strsplit(virusname,".fasta"))
 truename<-splitname[1]
 
 truenameRda= paste(truename, ".Rda", sep="")
-setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/Rda_Files")
+#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/Rda_Files")
 load(truenameRda)
 
 # graphs/ tables 
-setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
-source("Synonymous and nonsynonymous graph.R")
+#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
+source("R_scripts/BioInfo/Synonymous and nonsynonymous graph.R")
 SynNonsynAT(DF)
 SynNonsynCG(DF)
 
 
-source("MakesCPG Graph.R")
+source("R_scripts/BioInfo/MakesCPG Graph.R")
 CPGNoCPGAT(DF)
 
-source("CPG_Syn_Nonsyn_graph.R")
+source("R_scripts/BioInfo/CPG_Syn_Nonsyn_graph.R")
 comparing_CpG_Syn_Nonsyn (DF)
 
 # Wilcox test
 #source("stats.R")
 #Wilcox_test(DF)
 
-source("RyanWilcox.R")
+source("R_scripts/BioInfo/RyanWilcox.R")
 Wilcox_test(DF, truename)
 
-source("redoplot.R")
+source("R_scripts/BioInfo/redoplot.R")
 comparing_CpG_Syn_Nonsyn_new(DF, truename)
 
 #########
