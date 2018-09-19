05d8caccf106127675cd33fc0169f9f8a9cfcb27
RyanWinstead <ryanwinstead33@yahoo.com>
2018-09-18 19:28:16 -0700

    tables stuff pasted

--- R_scripts/graphs/MakesTables_Graphs.R
+++ R_scripts/graphs/MakesTables_Graphs.R
@@ -1,9 +1,13 @@
+Virus_info =read.csv("data/CpG_List.csv")
 for(i in 1:nrow(Virus_info)){
-read.csv("data/CpG_List.csv")
+  
   viruplace = paste('data/fasta/', Virus_info$name[i], sep="")
   name = as.character(Virus_info$name[i])
   splitname<-unlist(strsplit(as.character(Virus_info$name[i]),".fasta"))
   truename<-splitname[1]
+  DF=Tables(truename)
+  Pvalues=Wilcox_test(DF, truename)
+  makeTable(Pvalues, truename)
   
 }
 DF=Tables(truename)
