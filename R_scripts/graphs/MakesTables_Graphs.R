a479900f94fad294994d339eafcb2440a21c328f
Sarina Qin <sq134679@gmail.com>
2018-09-18 20:37:43 -0700

    new output path GT

--- R_scripts/graphs/MakesTables_Graphs.R
+++ R_scripts/graphs/MakesTables_Graphs.R
@@ -10,8 +10,11 @@ for(i in 1:nrow(Virus_info)){
   makeTable(Pvalues, truename)
   source("R_scripts/graphs/redoplot.R")
   comparing_CpG_Syn_Nonsyn_new(truename)
+  #bring back the image and set up so put two graphs in one page
+  truenamepng = paste("output/GT/",truename,".png",sep="")
+  png(truenamepng, width = 6.75, height = 6.75, units = "in", res= 300)
   
-}
+  }
 DF=Tables(truename)
 Pvalues=Wilcox_test(DF, truename)
 makeTable(Pvalues, truename)
