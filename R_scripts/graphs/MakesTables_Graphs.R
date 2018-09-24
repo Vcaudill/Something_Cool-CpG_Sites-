45e8b6a825bcdcd52b82a5c6aa5988c9875726ce
Sarina Qin <sq134679@gmail.com>
2018-09-23 21:45:30 -0700

    hi

--- R_scripts/graphs/MakesTables_Graphs.R
+++ R_scripts/graphs/MakesTables_Graphs.R
@@ -5,14 +5,21 @@ for(i in 1:nrow(Virus_info)){
   name = as.character(Virus_info$name[i])
   splitname<-unlist(strsplit(as.character(Virus_info$name[i]),".fasta"))
   truename<-splitname[1]
+  #source("R_scripts/Tables/HowToMakeWilcoxTables.R")
   DF=Tables(truename)
-  Pvalues=Wilcox_test(DF, truename)
+  Pvalues=Wilcox_test(DF, truename)#get error x must be numeric
   makeTable(Pvalues, truename)
   source("R_scripts/graphs/redoplot.R")
-  comparing_CpG_Syn_Nonsyn_new(truename)
+  redo<-comparing_CpG_Syn_Nonsyn_new(truename)
+  
   #bring back the image and set up so put two graphs in one page
   truenamepng = paste("output/GT/",truename,".png",sep="")
   png(truenamepng, width = 6.75, height = 6.75, units = "in", res= 300)
+  par(mfrow=c(1,2),mar=c(3.9, 4.1, 2.1, 0.8))
+  return(redo)
+  dev.off()
+  
+
   
   }
 DF=Tables(truename)
