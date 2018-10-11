d94555d506329fa76135a2a03cdead91b4dc399b
Sarina Qin <sq134679@gmail.com>
2018-10-11 00:51:46 -0700

    working takes random sequences from fasta file

---
+++ R_scripts/Tree/random_fasta_other.R
@@ -0,0 +1,51 @@
+ListofFastaFiles <-list.files("data/fasta/", pattern = "trim05", all.files = TRUE)
+library(seqinr) #package phylo for making trees
+library(ggtree)
+library(ape)
+samplesize = 100
+for (filename in ListofFastaFiles) {
+  print(filename)
+  if (filename == "~$fluenzaAvirus_NA_H1N1.fasta.mu.trim05") {
+    next
+  }
+  DataSet <- read.fasta(paste("data/fasta/", filename, sep = ""))
+  if (length(DataSet) < samplesize) {
+    DS <- sample(DataSet, size = length(DataSet))
+    # file<-paste("data/fasta/",filename,sep="")
+    # NAseqs<-ape::read.dna(file, format = "fasta")
+    # D<-dist.dna(NAseqs) #create a distance matrix
+    # 
+    # NJ<-nj(D) #create a neighbor joining tree
+    # ggtree(NJ)
+    # 
+  }else{
+    DS <- sample(DataSet, size = samplesize)
+    # file<-paste("data/fasta/",filename,sep="")
+    # NAseqs<-ape::read.dna(file, format = "fasta")
+    # D<-dist.dna(NAseqs) #create a distance matrix
+    # 
+    # NJ<-nj(D) #create a neighbor joining tree
+    # ggtree(NJ)
+    # 
+    
+    
+  }
+  
+}
+
+
+#DataSet <-read.fasta("InfluenzaAvirus_HA_H1N1.fasta.mu.trim05")
+DataSet <- read.fasta("DengueVirus4.fasta_pruned.mu.trim05")
+#DataSet <-read.fasta("InfluenzaBvirus_HA.fasta.mu.trim05")
+#View(DataSet)
+DS <- sample(DataSet, size = 50)
+#View(DS)
+write.fasta(DS, names(DS), file = "Den4")
+ListofFastaFiles <- list.files(pattern = "trim05")
+
+file <- paste("data/fasta", filename, sep = "")
+NAseqs <- ape::read.dna(file, format = "fasta")
+D <- dist.dna(NAseqs) #create a distance matrix
+
+NJ <- nj(D) #create a neighbor joining tree
+ggtree(NJ)
