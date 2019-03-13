2c192a743305987ecfbc6e7dfe989b2dd08c6f5a
Vcaudill <vcaudill@mail.sfsu.edu>
2019-03-12 22:31:48 -0700

    good luck

--- R_scripts/Fasta_nostopcodons.R
+++ R_scripts/Fasta_nostopcodons.R
@@ -3,28 +3,53 @@ library(seqinr)
 Virus_info<- read.csv("data/CpG_List.csv")
 
 fasta<-"BKpolyomavirus_VP1.fasta.mu.trim05"
-Short_fasta<-function(fasta){
+i = 1
+No_STOP_fasta<-function(fasta, i){
   fasta_file <-paste("data/fasta/",fasta,sep = "")
-  virus_basic <- read.fasta(fasta_file, as.string = TRUE)
+  virus_basic <- read.fasta(fasta_file)
   number_of_seqs <- length(virus_basic)
+  #cat("number_of_seqs", number_of_seqs)
   virus_align <- read.alignment(fasta_file, format = "fasta", forceToLower = T)
-  DF<-Freq(fasta)
-  splitname<-unlist(strsplit(fasta,".fasta"))
-  truename<-splitname[1]
-  DF$wtnt<-as.character(DF$wtnt)
-  DF$Virus<-(truename)
+  virus_consensus <- seqinr :: consensus(virus_align, method = "majority")
+  virus_consensus_matrix <- seqinr :: consensus(virus_align, method = "profile")
+  consensus_length <- length(virus_consensus)
+  number_column <- seq(1, consensus_length)
+  viruplace = paste('data/fasta/', Virus_info$name[i], sep="")
   
-  start = 1
-  end = 0
+  DF<-Freq(viruplace)
+  name = as.character(Virus_info$name[i])
+  splitname<-unlist(strsplit(as.character(Virus_info$name[i]),".fasta"))
+  truename<-splitname[1]
+  DF$wtnt_consensus<-as.character(DF$wtnt_consensus)
+  DF$Virus<-(truename)
+  #setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
+  
+  #  
+  # make sure that M is the first amino acid check all virus
+  start = Virus_info$start[i]
+  end = Virus_info$stop[i] # add three or sub
   DF<-DF[c(start:(nrow(DF)-end)),]
   DF$num<-(1:nrow(DF))
-  source("R_scripts/BioInfo/WTAA_consensus.R")
-  DF<-getWTAA(DF)
   
-  source("R_scripts/BioInfo/MUTAA.R")
-  DF<-getMUTAA(DF)
-  write.fasta(virus_basic, open = "a", names = truename, file = paste("output/ShortFasta/",truename,"_short.fasta",sep=""))
+  virus_basic1 <- read.fasta(fasta_file, as.string = TRUE)
+  write.fasta("", open = "w", names = "test", file = paste("output/NoStopFasta/",truename,"_short.fasta",sep=""))
+  splitname<-unlist(strsplit(fasta,".fasta"))
+  truename<-splitname[1]
+  for(i in 1:number_of_seqs){
+    print(i/number_of_seqs)
+    virus_DF$Differences[i]=adist(consensus,virus_basic1[[i]][1])
+    if(virus_DF$Differences[i] > 99){
+      next
+    }
+    write.fasta(virus_basic[i], open = "a", names = names(virus_basic[i]), file = paste("output/ShortFasta/",truename,"_short.fasta",sep=""))
     
   }
 }
-Short_fasta(fasta)
+
+
+#####
+
+# read in the file individulally a a g t a     not a sting aagta
+# cut off the begining if need and the stop 
+# translate * make sure there are no stop codons 
+# wrint in new file 
