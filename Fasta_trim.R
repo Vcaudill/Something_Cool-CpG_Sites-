f5e49414c65bdb8171cd94b747fb33dee0655e61
Vcaudill <vcaudill@mail.sfsu.edu>
2019-04-07 23:01:49 -0700

    things update

--- Fasta_trim.R
+++ Fasta_trim.R
@@ -7,47 +7,17 @@ Virus_info <- read.csv("data/CpG_List.csv")
 library(ape)
 library(seqinr)
 
-for(j in 1:nrow(Virus_info)){
-file<-Virus_info[j,1]
-path_file<-paste("data/fasta/",file, sep="")
-al<-read.fasta(path_file,as.string=TRUE)
-al2<-read.fasta(path_file,as.string=FALSE)
-number_of_nucs <-length(al2[[1]])
-end<-as.numeric(number_of_nucs)-as.numeric(Virus_info$stop[j])
-al.trimmed<-al[as.numeric(Virus_info$start[j]):end]
-
-
-pos<-c()
-for (i in 1: length(al)){
-  seq<-paste(al[i])
-  #split into 3 bases from the start (if frame1)
-  seq<-substring(seq,seq(1,nchar(seq),3), seq(3, nchar(seq),3) )
-  p<-which(seq =="taa"|seq=="tga"|seq=="tag")
-  pos<-c(pos,p)
-  
-}
-
-poss<-unique(pos)
-pos.remove<-c(poss*3-2, poss*3-1,poss*3) 
-
-algn1<-read.dna(path_file, format = "fasta")
-algn.trimmed<-algn1[,-pos.remove]
-output_file<-paste("output/NoStopFasta/",file,"_No_Stop.fasta",sep="")
-write.FASTA(algn.trimmed, file=output_file)
-}
-
-k=1
 for(k in 1:nrow(Virus_info)){
-file<-Virus_info[j,1]
-path_file<-paste("output/NoStopFasta/",file,"_No_Stop.fasta",sep="")
+file<-Virus_info[k,1]
+path_file<-paste("data/no_mid_stop/",file,".fasta",sep="")
 DataSet <- read.fasta(path_file)
 sample_size= 499
 if (Virus_info$SeqNumber[k] > sample_size) {
   DS <- sample(DataSet, size = sample_size)
   size = sample_size
-}else{
-  DS <- sample(DataSet, size = Virus_info$SeqNumber[k])
-  size = Virus_info$SeqNumber[k]
+  }else{
+    DS <- sample(DataSet, size = length(DataSet))
+    size = length(DataSet)
 }
 Fasta_trimmed_sample<-paste("output/FastaSample/",size,Virus_info$name[k],".fasta",sep="")
 write.fasta(DS,names(DS), file = Fasta_trimmed_sample)
