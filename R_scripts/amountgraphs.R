d9262b01ff60b45c1acd55ee8bbebc9c2364fd35
Vcaudill <vcaudill@mail.sfsu.edu>
2019-06-01 20:49:53 -0700

    fixed colors and shapes

--- R_scripts/amountgraphs.R
+++ R_scripts/amountgraphs.R
@@ -4,127 +4,124 @@ library(scales)
 library(plotrix)
 library(sfsmisc)
 library(gdata)
-Virus_info<- read.csv("data/CpG_List.csv")
+# Virus_info<- read.csv("data/CpG_List.csv")
+# 
+# data_points_df<- read.csv("output/alldatapoints.csv")
+# #data_points_df=data_points_df[-(27), ] 
+# data_points_df <- drop.levels(data_points_df[-(27), ]) 
+# tally<- read.csv("data/recount2.csv")
+tally<- read.csv("data/CpG_Newdata_tally.csv")
 
-data_points_df<- read.csv("output/alldatapoints.csv")
-#data_points_df=data_points_df[-(27), ] 
-data_points_df <- drop.levels(data_points_df[-(27), ]) 
-tally<- read.csv("data/recount2.csv")
-data_points_df$SynCpg=0
-data_points_df$NonSynCpg=0
-data_points_df$SynNonSyn=0
-for (i in 1:nrow(data_points_df)) {
- treat=data_points_df$Virus[i]
-  
-  print(treat)
- # treat=factor(data_points_df$Virus[i])
-  for (j in 1:nrow(tally)){
-    # splitnameAll<-unlist(strsplit(as.character(Virus_info$name[j]),".fasta"))
-    # splitname<-unlist(strsplit(as.character(splitnameAll[1]),"_"))
-   truename<-tally$Virus[j]
-    print(truename)
-   # truename=factor(tally$Virus[i])
-    if(treat==truename){
-      data_points_df$SynCpg[i]<-tally$A..G.Syn..CpG.v.NonCpG.[j]+tally$T..C.Syn..CpG.v.NonCpG.[j]
-      data_points_df$NonSynCpg[i]<-tally$A..G.NonSyn..CpG.v.NonCpG[j]+tally$T..C.NonSyn..CpG.v.NonCpG[j]
-      data_points_df$SynNonSyn[i]<-tally$A..G.Syn.v.NonSyn[j]+tally$T..C.Syn.v.NonSyn[j]
-      
-      
-    }
-  } 
-}
+# data_points_df$SynCpg=0
+# data_points_df$NonSynCpg=0
+# data_points_df$SynNonSyn=0
+# for (i in 1:nrow(data_points_df)) {
+#  treat=data_points_df$Virus[i]
+#   
+#   print(treat)
+#  # treat=factor(data_points_df$Virus[i])
+#   for (j in 1:nrow(tally)){
+#     # splitnameAll<-unlist(strsplit(as.character(Virus_info$name[j]),".fasta"))
+#     # splitname<-unlist(strsplit(as.character(splitnameAll[1]),"_"))
+#    truename<-tally$Virus[j]
+#     print(truename)
+#    # truename=factor(tally$Virus[i])
+#     if(treat==truename){
+#       data_points_df$SynCpg[i]<-tally$A..G.Syn..CpG.v.NonCpG.[j]+tally$T..C.Syn..CpG.v.NonCpG.[j]
+#       data_points_df$NonSynCpg[i]<-tally$A..G.NonSyn..CpG.v.NonCpG[j]+tally$T..C.NonSyn..CpG.v.NonCpG[j]
+#       data_points_df$SynNonSyn[i]<-tally$A..G.Syn.v.NonSyn[j]+tally$T..C.Syn.v.NonSyn[j]
+#       
+#       
+#     }
+#   } 
+# }
+# 
+# #Syn CpG vs Non-CpG
+#   
+tally$SynCpg<-tally$A..G.Syn..CpG.v.NonCpG.+tally$T..C.Syn..CpG.v.NonCpG.
+tally$NonSynCpg<-tally$A..G.NonSyn..CpG.v.NonCpG+tally$T..C.NonSyn..CpG.v.NonCpG
+tally$SynNonSyn<-tally$A..G.Syn.v.NonSyn+tally$T..C.Syn.v.NonSyn
 
-#Syn CpG vs Non-CpG
-  
+palette(alpha(c("red","deepskyblue1","green")))
 
-  png("output/amount/Syn.png", width = 6.75, height = 6.75, units = "in", res= 300)
+  png("output/amount/Syn_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
   #par(mfrow=c(2,2))#, bg = "darkseagreen1"
-  palette(alpha(c("red","green","deepskyblue1")))
-  plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+  plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=factor(tally$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
   legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
          col= c("deepskyblue1","green","red"), horiz= FALSE, 
          #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
          cex=1, pch = c(16,16,16))
   
-  png("output/amount/NonSyn.png", width = 6.75, height = 6.75, units = "in", res= 300)
-  palette(alpha(c("red","green","deepskyblue1")))
-  plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+  png("output/amount/NonSyn_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
+  plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=factor(tally$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
   legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
          col= c("deepskyblue1","green","red"), horiz= FALSE, 
          #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
          cex=1, pch = c(16,16,16))
-  png("output/amount/SynNonSyn.png", width = 6.75, height = 6.75, units = "in", res= 300)
-  palette(alpha(c("red","green","deepskyblue1")))
-  plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
+  png("output/amount/SynNonSyn_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
+  plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=factor(tally$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
   legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
          col= c("deepskyblue1","green","red"), horiz= FALSE, 
          #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
          cex=1, pch = c(16,16,16))
 dev.off()
 
-png("output/amount/alllogy.png", width = 6.75, height = 6.75, units = "in", res= 300)
+png("output/amount/alllogy_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
 par(mfrow=c(2,2))#, bg = "darkseagreen1"
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='y',col=factor(data_points_df$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+
+plot(tally$SeqNumber,tally$NucleotideNumber,log='y',col=factor(tally$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
        cex=1, pch = c(16,16,16))
 
 
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='y',col=factor(data_points_df$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+plot(tally$SeqNumber,tally$NucleotideNumber,log='y',col=factor(tally$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
        cex=1, pch = c(16,16,16))
 
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='y',col=factor(data_points_df$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
+plot(tally$SeqNumber,tally$NucleotideNumber,log='y',col=factor(tally$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
        cex=1, pch = c(16,16,16))
 dev.off()
 
-png("output/amount/alllogxy.png", width = 6.75, height = 6.75, units = "in", res= 300)
+png("output/amount/alllogxy_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
 par(mfrow=c(2,2))#, bg = "darkseagreen1"
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+
+plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=c("red","green","deepskyblue1")[as.factor(tally$SynCpg)],pch=c(19,15,17)[as.factor(tally$SynCpg)],cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 
 
+plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=c("red","green","deepskyblue1")[as.factor(tally$NonSynCpg)],pch=c(19,15,17)[as.factor(tally$NonSynCpg)],cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
-
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,log='xy',col=factor(data_points_df$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
+plot(tally$SeqNumber,tally$NucleotideNumber,log='xy',col=c("green","deepskyblue1")[as.factor(tally$SynNonSyn)],pch=c(15,17)[as.factor(tally$SynNonSyn)],cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
 plot(0, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
-       cex=1, pch = c(16,16,16))
+       cex=1, pch = c(17,15,19))
 dev.off()
 
-png("output/amount/allnolog.png", width = 6.75, height = 6.75, units = "in", res= 300)
+png("output/amount/allnolog_5_18.png", width = 6.75, height = 6.75, units = "in", res= 300)
 par(mfrow=c(2,2))#, bg = "darkseagreen1"
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,col=factor(data_points_df$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+
+plot(tally$SeqNumber,tally$NucleotideNumber,col=factor(tally$SynCpg),pch=19,cex=1.5, main="Syn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
        cex=1, pch = c(16,16,16))
 
 
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,col=factor(data_points_df$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
+plot(tally$SeqNumber,tally$NucleotideNumber,col=factor(tally$NonSynCpg),pch=19,cex=1.5, main="NonSyn CpG vs NonCpG",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
        cex=1, pch = c(16,16,16))
 
-palette(alpha(c("red","green","deepskyblue1")))
-plot(data_points_df$seq,data_points_df$nucl,col=factor(data_points_df$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
+plot(tally$SeqNumber,tally$NucleotideNumber,col=factor(tally$SynNonSyn),pch=19,cex=1.5, main="Syn vs NonSyn",xlab = "# of Sequences", ylab = "# of Nucletides")
 legend("bottomright",legend=c("Significant","Partially Significant","No Significance"),
        col= c("deepskyblue1","green","red"), horiz= FALSE, 
        #lty is type of line used,cex is size of legend, xpd allows legend to lie outside the plot,bty is type of box around legend       
