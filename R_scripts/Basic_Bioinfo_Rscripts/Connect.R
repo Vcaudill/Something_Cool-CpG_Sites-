f73771651d98bfb862c1775a9303280d618d24b4
Vcaudill <vcaudill@mail.sfsu.edu>
2019-07-02 22:28:41 -0700

    renameing and source fix

--- R_scripts/Basic_Bioinfo_Rscripts/Connect.R
+++ R_scripts/Basic_Bioinfo_Rscripts/Connect.R
@@ -4,18 +4,18 @@
 
 #loop using a csv to find the file name, strating and stoping points, and readign frame
 
-Virus_info<- read.csv("data/CpG_List_RyanVersion.csv")
-i=50
+Virus_info<- read.csv("data/list/Final_CpG_list.csv")
+
 # DataSet <-read.fasta("DengueVirus1.fasta_pruned.mu.trim05.txt")
 for(i in 1:nrow(Virus_info)){
   print(i)
-  source("R_scripts/BioInfo/Freq.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/Freq.R")
   # must place your file as a txt takes a few minutes 
   #setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/virus")
-  viruplace = paste('data/fasta/', Virus_info$name[i], sep="")
+  viruplace = paste(Virus_info$Fasta_File_Path[i],"/fasta/", Virus_info$File[i], sep="")
   DF<-Freq(viruplace)
-  name = as.character(Virus_info$name[i])
-  splitname<-unlist(strsplit(as.character(Virus_info$name[i]),".fasta"))
+  name = as.character(Virus_info$File[i])
+  splitname<-unlist(strsplit(as.character(Virus_info$File[i]),".fasta"))
   truename<-splitname[1]
   DF$wtnt_consensus<-as.character(DF$wtnt_consensus)
   DF$Virus<-(truename)
@@ -23,38 +23,39 @@ for(i in 1:nrow(Virus_info)){
   
   #  
   # make sure that M is the first amino acid check all virus
-  start = Virus_info$start[i]
-  end = Virus_info$stop[i]
-  DF<-DF[c(start:(nrow(DF)-end)),]
-  DF$num<-(1:nrow(DF))
+  #no longer needed
+  #start = Virus_info$start[i]
+  #end = Virus_info$stop[i]
+  #DF<-DF[c(start:(nrow(DF)-end)),]
+  #DF$num<-(1:nrow(DF))
   
   
-  source("R_scripts/BioInfo/WTAA_consensus.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/WTAA_consensus.R")
   DF<-getWTAA(DF)
   
-  source("R_scripts/BioInfo/MUTAA.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/MUTAA.R")
   DF<-getMUTAA(DF)
   
-  source("R_scripts/BioInfo/Drastic_AA_Change.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/Drastic_AA_Change.R")
   DF<-big_aa_change(DF)
   
-  source("R_scripts/BioInfo/CPG_Function.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/CPG_Function.R")
   DF<-CPG_site(DF)
   
-  source("R_scripts/BioInfo/SynNonSyn.R")
+  source("R_scripts/Basic_Bioinfo_Rscripts/SynNonSyn.R")
   DF<-synFunction(DF)
   
   #How to save data
-  truenameRda= paste('data/Rda/', truename, ".Rda", sep="")
-  save("virus" = DF,file= truenameRda)
-  truenameCSV= paste('data/Csv/', truename, ".csv", sep="")
+  #truenameRda= paste('data/Rda/', truename, ".Rda", sep="")
+  #save("virus" = DF,file= truenameRda)
+  truenameCSV= paste(Virus_info$Fasta_File_Path[i],"/Csv/", truename, sep="")
   write.csv(DF, file = truenameCSV)
 }
 
-source("R_scripts/BioInfo/SynNonSyn.R")
+source("R_scripts/Basic_Bioinfo_Rscripts/SynNonSyn.R")
 DF<-synFunction(DF)
 
-source("R_scripts/BioInfo/CPG_Function.R")
+source("R_scripts/Basic_Bioinfo_Rscripts/CPG_Function.R")
 DF<-CPG_site(DF)
 #How to load data
 virusname = 'EnterovirusB_VP2.fasta_pruned.mu.trim05.txt'
