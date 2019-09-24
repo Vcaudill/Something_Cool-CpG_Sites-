c971de155c493ae2f363df34316386aa04303186
RyanWinstead <ryanwinstead33@yahoo.com>
2019-09-24 15:31:01 -0700

    eh

--- R_scripts/Basic_Bioinfo_Rscripts/SlimtoR.R
+++ R_scripts/Basic_Bioinfo_Rscripts/SlimtoR.R
@@ -1,7 +1,26 @@
-slimdir <- "~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/data/Slim"
+slimdir <- "/Users/ryanwinstead/Something_Cool-CpG_Sites-/data/Slim"
 setwd(slimdir)
+
 simdata <-list.files(slimdir)
+
 for (file in simdata){ 
-  DataSet <-read.delim(file, sep =" ")
+  classes <- c("positions:")
+  positions <- as.list(strsplit(gsub("\n", " ", readChar(file, file.info(file)$size)), " "))
+  for (entry in positions){
+    if (grepl("^[[:digit:]]+L",entry)){
+      positions[[entry]] <- NULL
+      print(entry)
+    }
+  }
+  #positions[["//"]] <- NULL
+  #DF <-read.table(file, sep =" ", colClasses = classes, header=TRUE, fill = TRUE)
+  DF <- positions
+  print(positions)
   break
 }
+
+
+
+#bigstring <-readChar(file, file.info(file)$size)
+#bigstrings <- gsub("\n", " ", bigstring)
+#positions <- as.list(strsplit(bigstrings, " "))
